be491ede316b0aa31262b881197de85e7e778865
duresaguye <dureguye2@gmail.com>
2025-01-25 01:55:05 +0300

    home page updated

--- src/app/page.js
+++ src/app/page.js
@@ -1,77 +1,58 @@
-/* eslint-disable react/no-unescaped-entities */
+
 "use client";
 
-import React, { useState } from 'react';
-import { useRouter } from 'next/navigation';
+import { useRef } from 'react';
+import Sidebar from "@/components/Sidebar";
+import Testimonials from "@/components/Testimonials";
+import HowItWorks from "@/components/HowItWorks";
+import CallToAction from '@/components/CallToAction';
 
-const Login = () => {
-    const router = useRouter();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState('');
+export default function HomePage() {
+  // Create a reference for the How It Works section
+  const howItWorksRef = useRef(null);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const result = await signIn('credentials', {
-            redirect: false,
-            email,
-            password,
-        });
+  // Function to scroll to the How It Works section
+  const scrollToHowItWorks = () => {
+    if (howItWorksRef.current) {
+      howItWorksRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
 
-        if (result.ok) {
-            router.push('/');
-        } else {
-            setError('Invalid email or password');
-        }
-    };
-
-    return (
-        <div className="min-h-screen bg-white text-white flex items-center justify-center">
-            <div className="relative z-10 w-full max-w-md bg-opacity-90 p-8 rounded-lg shadow-lg text-white">
-                <form onSubmit={handleSubmit} className="space-y-6 bg-gray-800 p-6 rounded-lg shadow-lg mt-10">
-                    {error && <p className="text-red-500">{error}</p>}
-                    <h1 className="text-4xl font-bold mb-6 text-center">Login</h1>
-                    <div>
-                        <label htmlFor="email" className="block text-sm font-medium text-gray-300 mb-1">Email</label>
-                        <input
-                            type="email"
-                            id="email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="w-full px-3 py-2 border border-gray-600 rounded-md bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            placeholder="Enter your email"
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="password" className="block text-sm font-medium text-gray-300 mb-1">Password</label>
-                        <input
-                            type="password"
-                            id="password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="w-full px-3 py-2 border border-gray-600 rounded-md bg-gray-700 text-gray-200 placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                            placeholder="Enter your password"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 w-full transition duration-300"
-                    >
-                        Login
-                    </button>
-                    <p className="mt-4 text-gray-300">Don&apos;t have an account?</p>
-                    <button
-                        onClick={() => router.push('/signup')}
-                        className="bg-indigo-600 text-white px-4 py-2 rounded-md hover:bg-indigo-700 w-full transition duration-300"
-                    >
-                        Signup
-                    </button>
-                </form>
-            </div>
+  return (
+    <div className="flex flex-col min-h-screen ">
+      <div className="flex flex-1">
+        <Sidebar />
+        <div className="relative flex flex-col items-center justify-center flex-1 bg-cover bg-center"
+             style={{ backgroundImage: "url('/background.webp')" }} 
+        >
+          {/* Background blur overlay */}
+          <div className="absolute inset-0 bg-black opacity-60 backdrop-blur-md"></div>
+          
+          <h1 className="text-4xl font-bold text-white text-center mb-4 relative z-10">
+            We’re solving poverty and plastic pollution with one transformative idea
+          </h1>
+          <p className="text-5xl font-bold text-white text-center mb-8 relative z-10">
+            Plastic as Money
+          </p>
+          
+        
+  
+          <button 
+            onClick={scrollToHowItWorks}
+            className="mt-4 px-4 py-2 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition relative z-10"
+          >
+       Learn More
+          </button>
         </div>
-    );
-};
+      </div>
 
-export default Login;
+      {/* How It Works Section with ref */}
+      <div ref={howItWorksRef}>
+        <HowItWorks />
+      </div>
+
+      <Testimonials />
+      <CallToAction />
+    </div>
+  );
+}
